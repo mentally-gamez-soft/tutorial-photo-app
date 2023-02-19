d173d0f16e7107b01a8b171f1fddac442745caab
mentally.gamez.soft <mentally.gamez.soft@gmail.com>
2023-02-19 12:51:29 +0000

    initialisation for project vuejs

---
+++ src/views/HomePage.vue
@@ -0,0 +1,42 @@
+<template>
+  <ion-page>
+    <ion-header :translucent="true">
+      <ion-toolbar>
+        <ion-title>Inbox</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content :fullscreen="true">
+      <ion-refresher slot="fixed" @ionRefresh="refresh($event)">
+        <ion-refresher-content></ion-refresher-content>
+      </ion-refresher>
+
+      <ion-header collapse="condense">
+        <ion-toolbar>
+          <ion-title size="large">Inbox</ion-title>
+        </ion-toolbar>
+      </ion-header>
+
+      <ion-list>
+        <MessageListItem v-for="message in messages" :key="message.id" :message="message" />
+      </ion-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script setup>
+import {
+  IonContent,
+  IonHeader,
+  IonList,
+  IonPage,
+  IonRefresher,
+  IonRefresherContent,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/vue';
+import MessageListItem from '@/components/MessageListItem.vue';
+import { getMessages, Message } from '@/data/messages';
+import { ref } from 'vue';
+
+</script>
